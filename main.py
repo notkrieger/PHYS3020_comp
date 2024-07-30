d42b7c91c5c5024713982a2fa90fc070a4253050
Rhys Tyne <tynerhys@gmail.com>
2024-07-30 10:33:19 +1000

    new beginnings

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 import numpy as np
 
+k = 1.38 * 10**-23  # Boltzmann constant
+N = 10  # number of dipoles in the system
+epsilon = 1  # energy contribution factor??
+T = 1  # temperature
+
+
+# main functionality
+def __main__():
+    pass
+
+__main__()
