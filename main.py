925aa903cd264ed27f3eff1b8a071afb355fd0ef
Rhys Tyne <tynerhys@gmail.com>
2024-08-21 12:53:50 +1000

    fixed entropy error

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import time
 
 
 k = 1  # Boltzmann constant -- currently trying to use "natural units"
-N = 250  # number of dipoles in the system
+N = 100  # number of dipoles in the system
 epsilon = 1  # energy contribution factor??
 initial_T = 2  # temperature
 spin_probability = 0.5  # when initialising if random.random >= spin_probability, set spin to 1, else -1
@@ -22,7 +22,7 @@ def visualise(state, temp, string):
     plt.figure(figsize=(10, 2))
     seaborn.heatmap([state]*2, cmap="Greys", cbar=False, xticklabels=False,
                     yticklabels=False)  # had make it 2d for heatmap to work :(
-    plt.title(string + " spin state")
+    plt.title(string + " spin state at temperature: " + str(temp))
     plt.show()
 
 
@@ -58,7 +58,7 @@ def solveU(state):
 # inputs:
     # state - some microstate
     # beta - defined as 1/kT, used to determine a probability
-
+    # lastU - internal energy of the previous state, used to speed up calc for nextU
 # returns:
     #   - next state -- may have one flipped dipole compared to state
 def metropolis(state, beta, lastU): # did not have to speed up as much because N << N^2 (for N = 100)
@@ -83,20 +83,20 @@ def find_multiplicity(state):
     for dipole in state:
         if dipole > 0:
             up_count += 1
-    return float(comb(len(state), up_count))
+    #print("up percent: " + str(up_count/N))
+    return float(comb(N, up_count))
 
 
 def model(temp): # one dimensional Ising Model
     # variables
-    total_steps = 1000 * N
+    total_steps = 1000 * N # is there some way to make a distribution
+    # so all dipoles definitely get selected?
     Us = np.linspace(0, total_steps, total_steps)
     Us2 = np.linspace(0, total_steps, total_steps)
+    Ss = np.linspace(0, total_steps, total_steps)
     beta = 1/(k*temp)
-    totalU = 0
-    totalU2 = 0
     totalm = 0
 
-    end = int(total_steps/1000) # get last "end" elements to calculate time average
     # initialise spins
     spins = np.random.random(N)
     for i in range(N):
@@ -109,9 +109,9 @@ def model(temp): # one dimensional Ising Model
     Us[0] = solveU(spins)
     Us2[0] = solveU(spins) ** 2
     for i in range(total_steps):
+        Ss[i] = np.log(find_multiplicity(spins))
         #print(Us[i])
         spins, next_U = metropolis(spins, beta, Us[i])
-        totalU += next_U
 
         if i + 1 >= total_steps:
             continue
@@ -124,14 +124,18 @@ def model(temp): # one dimensional Ising Model
     # calculate stuff for plots
     U_ave = np.mean(Us) # time average value of U : ⟨U⟩
 
-    S = k * np.log(find_multiplicity(spins)) # entropy S = k ln(g)
+    S = np.mean(Ss[-int(total_steps / 10)]) # entropy S = k ln(g), do I use S = -k * sum(P(i) * ln (P(i))?
     f = U_ave - temp * S # free energy F = U - TS
     # Var(U) = ⟨U^2⟩ - ⟨U⟩^2
     # only use last 10% of time steps for accurate results
     # this just worked idk
     c = np.var(Us[-int(total_steps/10):])/(temp**2)
+    up_count = 0
+    for i in range(N):
+        if spins[i] > 0:
+            up_count += 1
 
-    m = np.mean(spins) # average spin m = M/mu*N = s_bar
+    m = abs(np.mean(spins)) # average spin m = M/mu*N = s_bar
 
     #visualise(spins, temp, "Final") # visualise final state
     return U_ave/N, f/N, S/N, c/N, m
@@ -189,9 +193,7 @@ def make_plots(temps_sim, temps_exact):
         start = time.time()
         print(trial)
         for i in range(num_sim_temps):
-
             temp = temps_sim[i]
-            print(temp)
             results = model(temp)
             us_sim[trial][i] = results[0]
             fs_sim[trial][i] = results[1]
@@ -209,6 +211,7 @@ def make_plots(temps_sim, temps_exact):
         fs.append(-epsilon-k*temp*np.log(1 + np.exp(-2*epsilon*beta)))
         Ss.append(epsilon/temp*(1 - np.tanh(beta*epsilon)) + k*np.log(1 + np.exp(-2*epsilon*beta)))
         cs.append(epsilon**2*beta/(temp * np.cosh(beta*epsilon)**2))
+    print(Ss_sim)
     """
     for i in range(num_sim_temps):
         plt.hist(m_aves[i], range=[-1, 1], bins=50)
@@ -235,6 +238,7 @@ def make_plots(temps_sim, temps_exact):
     plt.ylabel('f')
     plt.xlabel("temperature")
     plt.show()
+    """
     #plot S
     plt.plot(temps_exact, Ss, label="exact")
     plt.errorbar(temps_sim, ave(Ss_sim), ls="--", yerr=error(Ss_sim), ecolor="k", label="sim")
@@ -253,14 +257,15 @@ def make_plots(temps_sim, temps_exact):
     plt.ylabel('c')
     plt.xlabel("temperature")
     plt.show()
-    """
+    
     # plot m
     plt.errorbar(temps_sim, ms_sim)
     plt.title("plot of m against T")
     plt.ylabel('m')
     plt.xlabel("temperature")
-    #plt.show()
+    plt.show()
     """
+
 
 
 make_plots(temperatures_sim, temperatures_exact)
